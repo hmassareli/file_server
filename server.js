a4347d08964326b04310bba01350b947e58742b3
Henrique Massareli <henrique@oneseventech.com>
2025-10-06 16:39:58 -0300

    create simple file server

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const mediaPath = "/app/media"; // vai ser o volume que você vai montar depois
+
+// Servir os arquivos estáticos
+app.use("/media", express.static(mediaPath));
+
+// Rota para listar os arquivos
+app.get("/list", (req, res) => {
+  const files = fs.readdirSync(mediaPath);
+  res.json(files);
+});
+
+// Porta do container (use a mesma no EasyPanel)
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
